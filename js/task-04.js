ca8dfde9e8308797577a8f0b16a399aa34fa4f6d
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-05-03 01:33:39 +0300

    5taks

--- js/task-04.js
+++ js/task-04.js
@@ -1,14 +1,14 @@
 let counterValue = 0;
 
-const buttonDecr = document.querySelector('button[data-action="decrement"]');
-const buttonInc = document.querySelector('button[data-action="increment"]');
+const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+const buttonIncrement = document.querySelector('button[data-action="increment"]');
 const valueEl = document.querySelector('#value')
 
-buttonDecr.addEventListener('click', function () {
+buttonDecrement.addEventListener('click', function () {
   counterValue -= 1;
   valueEl.textContent = counterValue;
 })
-buttonInc.addEventListener('click', function () {
+buttonIncrement.addEventListener('click', function () {
   counterValue += 1;
   valueEl.textContent = counterValue;
 })
