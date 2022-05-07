c111930d90a00394e61f52299e285356b0e75c93
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-05-07 16:56:58 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+    const form= document.querySelector('.login-form');
+    const email= document.querySelector('input[name="email"]');
+    const password= document.querySelector('input[name="password"]');  
 
+form.addEventListener('submit', event => { 
+    event.preventDefault(); //не перезагружає сторінку
+
+    if (event.currentTarget.elements.password.value === "" || event.currentTarget.elements.email.value === "") {
+        return alert('All fields must be filled');
+    }
+
+    const mail = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value
+    const formData = {
+        mail,
+        password,
+    }
+    console.log(formData);
+    event.currentTarget.reset();//очищує значення полів
+})
